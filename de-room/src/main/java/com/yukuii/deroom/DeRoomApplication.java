b25c6f869bf55c92f88ae464473919b744e68b29
Yukuiii <472619563@qq.com>
2025-02-22 21:33:58 +0800

    feat: 添加 DeRoomApplication 启动类
    - 新增 DeRoomApplication 类作为项目启动入口
    - 使用 SpringBootApplication 注解配置自动装配
    - 实现 main 方法启动 Spring Boot 应用

---
+++ de-room/src/main/java/com/yukuii/deroom/DeRoomApplication.java
@@ -0,0 +1,13 @@
+package com.yukuii.deroom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeRoomApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeRoomApplication.class, args);
+    }
+
+}
